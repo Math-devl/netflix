39d09dad5ce68399adaa2427dd3bebba8a2ac99a
MatDV <matdv@MatDVs-MBP.lan>
2024-08-20 15:20:18 +0200

    display issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import movies from "./assets/movies.json";
 
 function App() {
   //console.log(movies[0]); // {category: 'Reprendre avec', images: Array(27)}
-  const moviesArr = movies.map((elem, index) => {
-    // console.log(elem);
-    // console.log(elem.category);
-    return <h1 key={index}>{elem.category}</h1>;
-  });
-  return <>{moviesArr}</>;
+  return (
+    <>
+      {movies.map((elem, index) => {
+        //1er tour de .map pour récupérer la 1ère catégorie
+        console.log(elem.category);
+        <h1 key={index}>{elem.category}</h1>;
+        //2eme tour de .map pour récupérer les urls de chaque catégory
+        {
+          elem.images.map((elemUrl, indexImg) => {
+            console.log("lien de chaque image ===>", elemUrl);
+            <img key={indexImg} src={elemUrl} alt="" />;
+          });
+        }
+      })}
+    </>
+  );
 }
 
 export default App;
