bd1e701a7cd7ea4713bb32dfdadf5b55871d9d37
MatDV <matdv@MatDVs-MBP.lan>
2024-08-20 16:12:41 +0200

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,17 @@ import "./App.css";
 import movies from "./assets/movies.json";
 
 function App() {
-  //console.log(movies[0]); // {category: 'Reprendre avec', images: Array(27)}
   return (
     <>
       {movies.map((elem, index) => {
-        //1er tour de .map pour récupérer la 1ère catégorie
-        console.log(elem.category);
-        <h1 key={index}>{elem.category}</h1>;
-        //2eme tour de .map pour récupérer les urls de chaque catégory
-        {
-          elem.images.map((elemUrl, indexImg) => {
-            console.log("lien de chaque image ===>", elemUrl);
-            <img key={indexImg} src={elemUrl} alt="" />;
-            return (
-              <div>
-                <h1 key={index}>{elem.category}</h1>;
-                <img key={indexImg} src={elemUrl} alt="" />;
-              </div>
-            );
-          });
-        }
+        return (
+          <div key={index}>
+            <h1>{elem.category}</h1>
+            {elem.images.map((elemUrl, indexImg) => {
+              return <img key={indexImg} src={elemUrl} alt="" />;
+            })}
+          </div>
+        );
       })}
     </>
   );
